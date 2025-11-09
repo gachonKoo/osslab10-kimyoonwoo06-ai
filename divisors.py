5a4f1de483c821a6210fffcb30af60325951e7e9
Yoonwoo <kimyo@likeforyun.com>
2025-11-09 21:44:54 +0900

    fix autograding

--- divisors.py
+++ divisors.py
@@ -4,3 +4,10 @@ def get_divisors(n):
         if n % i == 0:
             result.append(i)
     return result
+
+if __name__ == "__main__":
+    import sys
+    n = int(sys.argv[1])   # 명령줄에서 숫자 받아오기
+    divisors = get_divisors(n)
+    for d in divisors:
+        print(d)
